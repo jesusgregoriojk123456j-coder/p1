0725409dca7c5cbf65852fe074ac2456d3ac89e5
jesusR <jesus.gregorio.jk123456j@gmail.com>
2026-05-11 07:59:50 -0600

    Update DB config for production

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'sigelab_db',
-    password: '12345', // Cambia por tu contraseña
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
